4e55134202e766ea9f6d0e17b1a9bbc84bfcaa55
Valihan <valihanburhanov@yandex.ru>
2023-01-15 09:53:39 +0300

    Решил 3 задачу в ветке Task3

--- c#hw1.cs
+++ c#hw1.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.Write("Введите число которое хотите проверить: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number%2 == 0)
+{
+    Console.Write($"{number} -> да");
+}
+else if (number%2 !=0)
+{
+    Console.Write($"{number} -> нет");
+}
+else
+{
+    Console.Write("Нормальное число впиши, понял?");
+}
