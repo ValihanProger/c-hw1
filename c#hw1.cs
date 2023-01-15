08d37d1fc8a27de61f13e1889df752d0bd14694f
Valihan <valihanburhanov@yandex.ru>
2023-01-15 09:48:02 +0300

    Решил вторую задачу в ветке task2

--- c#hw1.cs
+++ c#hw1.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+
+Console.Write("Введите первое число: ");
+int first = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int second = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int third  = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (first>second)
+{
+    max=first;
+    if (max>third)
+    {
+        Console.Write($"Максимальное из трех чисел = {max}");
+    }
+    else
+    {
+        max = third;
+        Console.Write($"Максимальное из трех чисел = {max}");
+    }
+}
+else 
+{
+    max=second;
+    if (max>third)
+    {
+        Console.Write($"Максимальное из трех чисел = {max}");
+    }
+    else
+    {
+        max=third;
+        Console.Write($"Максимальное из трех чисел = {max}");
+    }
+}
+
+
